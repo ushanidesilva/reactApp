730c35d2a73fad0e7f0033b680f81b1843b89690
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-22 02:16:03 +0530

    First Commit bla bla

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends React.Component {
+    render() { 
+        return <div>
+            <h1>Hello World!</h1>
+            <button>OK</button>
+        </div>;
+    }
+}
+ 
+export default Counter;
